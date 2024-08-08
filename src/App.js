b3a2fd00f585ac2012ec358287935fea6639ede4
Boubamahir2 <amamahir1@gmail.com>
2024-08-08 22:06:11 +0200

    source code changed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import About from "./pages/About/About";
-import Lodging from "./pages/Lodging/Lodging";
-import NotFound from "./pages/NotFound/NotFound";
-import Layout from "./pages/Layout/Layout";
-import apparts from "./assets/data/appartements.json"
-import dataAbout from "./assets/data/aboutData.json"
+import {
+  AboutUs,
+  Chef,
+  Findus,
+  Footer,
+  Gallery,
+  Header,
+  Intro,
+  Laurels,
+  SpecialMenu,
+} from './containers';
 
-function App() {
-  return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route index element={<Home apparts={apparts} />} />
-        <Route path="/home" element={<Home apparts={apparts} />} />
-        <Route path="/about" element={<About dataAbout={dataAbout} />} />
-        <Route
-          path="/lodging/:id"
-          element={<Lodging apparts={apparts} />}
-        />
-        <Route path="/error" element={<NotFound />} />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
-  );
-}
+import { Navbar } from './components';
+import './App.css';
+
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <Findus />
+    <Footer />
+  </div>
+);
 
 export default App;
--- src/components/Tag/Tag.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Tag({tag}) {
-  return (
-    <li className='tag'>{tag}</li>
-  )
-}
-
-export default Tag
