54d77cd1bf8c8475bcd64935e0f6f87b1f1d549a
Boubamahir2 <amamahir1@gmail.com>
2024-08-09 01:02:39 +0200

    modified:   eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,23 @@
-export default [
-  {
-    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
-    rules: {
-      indent: ['error', 2], // Enforce consistent indentation with 2 spaces
-      quotes: ['error', 'single'],  // Enforce single quotes
-      semi: ['error', 'always'], // Require semicolons
+export default {
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
   },
-];
+  rules: {
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        // TypeScript-specific rules
+      },
+    },
+  ],
+};
