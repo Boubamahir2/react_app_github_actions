45c562676d654eb02733b8780898aa307feab3dc
Boubamahir2 <amamahir1@gmail.com>
2024-08-09 01:20:11 +0200

    modified:   eslint.config.js 	modified:   postcss.config.cjs 	modified:   vite.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,16 @@
-export default {
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
+export default [
+  {
+    files: ['**/*.js', '**/*.cjs'],
+    rules: {
+      semi: 'error',
+      'no-unused-vars': 'error',
     },
   },
-  rules: {
-    indent: ['error', 2],
-    quotes: ['error', 'single'],
-  },
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        // TypeScript-specific rules my rules
-        indent: ['error', 2], // Enforce consistent indentation with 2 spaces
-        quotes: ['error', 'single'], // Enforce single quotes
-        semi: ['error', 'always'], // Require semicolons
-      },
+  {
+    files: ['**/*.js'],
+    rules: {
+      'no-undef': 'error',
+      semi: 'warn',
     },
-  ],
-};
+  },
+];
