8170e9aab843b247041c901c6c0a9d4494d25bee
Boubamahir2 <amamahir1@gmail.com>
2024-08-09 01:01:38 +0200

    modified:   eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ export default [
     files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
     rules: {
       indent: ['error', 2], // Enforce consistent indentation with 2 spaces
-      quotes: ['error', 'single'], // Enforce single quotes
+      quotes: ['error', 'single'],  // Enforce single quotes
       semi: ['error', 'always'], // Require semicolons
     },
   },
